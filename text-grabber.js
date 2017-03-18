5301ca7cd47d4a9a62fcea33d694b630e367cf90
Connor <cpk2000x@gmail.com>
2017-03-18 10:35:47 -0700

    Prob bork

--- text-grabber.js
+++ text-grabber.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-var server = app.listen(8000, function () {
+var server = app.listen(80, function () {
    var host = server.address().address
    var port = server.address().port
 })
