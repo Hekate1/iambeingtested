744dc9a6ad71f9814b29e9a0eeb218eedbc6e7b9
Connor <cpk2000x@gmail.com>
2017-03-18 11:21:44 -0700

    asgd

--- text-grabber.js
+++ text-grabber.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-var server = app.listen(80, function () {
+var server = app.listen(process.env.PORT, function () {
    var host = server.address().address
    var port = server.address().port
 })
