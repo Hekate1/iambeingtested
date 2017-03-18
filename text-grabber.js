f6e2e30624143920100a7bf4583f148f187fa801
Connor <cpk2000x@gmail.com>
2017-03-18 01:17:02 -0700

    Sentiment section works

--- text-grabber.js
+++ text-grabber.js
@@ -1,2 +1,19 @@
-var textInBox = document.getElementById('textBox').value;
-console.log(textInBox);
+//var textInBox = document.getElementById('textBox').value;
+//console.log(textInBox);
+
+var AlchemyLanguageV1 = require('watson-developer-cloud/alchemy-language/v1');
+
+var alchemy_language = new AlchemyLanguageV1({
+  api_key: '3c1d6ef90c66d257d834d3766cf0cd6d49ccdfa6'
+});
+
+var params = {
+  text: 'IBM Watson won the Jeopardy television show hosted by Alex Trebek'
+};
+
+alchemy_language.sentiment(params, function (err, response) {
+  if (err)
+    console.log('error:', err);
+  else
+    console.log(JSON.stringify(response, null, 2));
+});
