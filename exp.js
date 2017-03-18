61d6bf05b34031e401eeeb4273f02d80f601e906
Kenneth McNelly <kenneth.mcnelly@menloschool.org>
2017-03-18 06:00:23 -0700

    Experiment js file
    did the var string paragraph thingy

---
+++ exp.js
@@ -0,0 +1,10 @@
+var title = "Pizza Leads to Cancer"
+var author = "Conner Kennedy"
+var development = "discoveries"
+var situation = "national ebola pandemic"
+var statistic = "5% increase in pizza related deaths"
+var reaction = "a national pizza ban is in order"
+
+var result = ("The article, '" + title + " by " + author + " discusses the recent " + development +
+    " regarding the " + situation + ". " + author + " addresses the " + statistic +
+     " as reason for concern. He states that " + reaction)
