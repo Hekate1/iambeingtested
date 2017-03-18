38ce38093a1568a1a26473f9c669659ee4fd60c9
Connor <cpk2000x@gmail.com>
2017-03-18 15:47:56 -0700

    age

--- text-grabber.js
+++ text-grabber.js
@@ -190,7 +190,7 @@ app.post('/sent-text', function (req, res) {
           
         //REPLY HERE
         var reply = "This article, " + title + " by " + author + ", expresses " + degree + " " + emotion + ". It is a " + taxonomy + " article that discusses " + concepts + ".";
-        res.status(200).send("\n" + title + " \n" + authors + " \n" + taxonomy + " \n" + concepts + " \n" + entities + " \n" + keywords + " \n" + emotion + " \n" + sentiment);
+        res.status(200).send(reply);
       }
         
     });
