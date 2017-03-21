c0a8b3e6813d4e280a8d30a22a72f414824eb9bd
Connor <cpk2000x@gmail.com>
2017-03-20 18:02:55 -0700

    Test8

--- text-grabber.js
+++ text-grabber.js
@@ -9,7 +9,7 @@ var server = app.listen(process.env.PORT || 1800, function () {
 
 app.use(express.static(__dirname));
 
-app.post('/sent-text', function (data, res) {
+app.post('/sent-text', function (req, res) {
     console.log("GOT HERE")
     cosole.log(data)
     var AlchemyLanguageV1 = require('watson-developer-cloud/alchemy-language/v1');
