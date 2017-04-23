3ad819bd92b09f8298594d9fd05c2fba6d4f6f6f
Connor <cpk2000x@gmail.com>
2017-04-22 20:06:30 -0700

    h

--- being-tested.js
+++ being-tested.js
@@ -1,6 +1,7 @@
-var express = require('express');
-var fs = require("fs");
-var app = express();
+var express = require('express')
+var fs = require("fs")
+var app = express()
+var globalArray = []
 
 var server = app.listen(process.env.PORT || 1800, function () {
    var host = server.address().address
@@ -10,8 +11,9 @@ var server = app.listen(process.env.PORT || 1800, function () {
 app.use(express.static(__dirname));
 
 app.post('/array', function (data, res) {
-    console.log("test1")
     console.log(data.headers.myarray)
-    JSON.parse(data.headers.myarray)
+    var array = JSON.parse(data.headers.myarray)
+    
+    
     res.status(200).send()
 })
