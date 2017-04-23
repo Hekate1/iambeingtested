62e5b09aa78369f46b2e5be223d7b5c5254e1811
Connor <cpk2000x@gmail.com>
2017-04-22 20:20:10 -0700

    k

--- being-tested.js
+++ being-tested.js
@@ -13,7 +13,7 @@ app.use(express.static(__dirname));
 app.post('/array', function (data, res) {
     var array = JSON.parse(data.headers.myarray)
     var m1 = 5/((array[0][0] + array[1][0] + array[2][0] + array[3][0] + array[4][0])/5)
-    array[0][0] * m1
+    array[0][0] *= m1
     console.log(array[0][0])
     res.status(200).send()
 })
