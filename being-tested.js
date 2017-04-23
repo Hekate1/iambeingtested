99a02bd723e8630d2788813327607a3fb45aac4c
Connor <cpk2000x@gmail.com>
2017-04-22 20:26:12 -0700

    n

--- being-tested.js
+++ being-tested.js
@@ -12,9 +12,43 @@ app.use(express.static(__dirname));
 
 app.post('/array', function (data, res) {
     var array = JSON.parse(data.headers.myarray)
+    
     var m1 = 5/((array[0][0] + array[1][0] + array[2][0] + array[3][0] + array[4][0])/5)
-    console.log(m1)
     array[0][0] *= m1
-    console.log(array[0][0])
+    array[1][0] *= m1
+    array[2][0] *= m1
+    array[3][0] *= m1
+    array[4][0] *= m1
+    
+    var m2 = 5/((array[0][1] + array[1][1] + array[2][1] + array[3][1] + array[4][1])/5)
+    array[0][1] *= m1
+    array[1][1] *= m1
+    array[2][1] *= m1
+    array[3][1] *= m1
+    array[4][1] *= m1
+    
+    var m3 = 5/((array[0][2] + array[1][2] + array[2][2] + array[3][2] + array[4][2])/5)
+    array[0][2] *= m1
+    array[1][2] *= m1
+    array[2][2] *= m1
+    array[3][2] *= m1
+    array[4][2] *= m1
+    
+    var m4 = 5/((array[0][3] + array[1][3] + array[2][3] + array[3][3] + array[4][3])/5)
+    array[0][3] *= m1
+    array[1][3] *= m1
+    array[2][3] *= m1
+    array[3][3] *= m1
+    array[4][3] *= m1
+    
+    var m5 = 5/((array[0][4] + array[1][4] + array[2][4] + array[3][4] + array[4][4])/5)
+    array[0][4] *= m1
+    array[1][4] *= m1
+    array[2][4] *= m1
+    array[3][4] *= m1
+    array[4][4] *= m1
+    
+    console.log(array)
+    
     res.status(200).send()
 })
