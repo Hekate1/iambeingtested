83c4ae3b6a7fe1c6414eb632d54788ad93580d6e
Connor <cpk2000x@gmail.com>
2017-04-22 20:29:17 -0700

    o

--- being-tested.js
+++ being-tested.js
@@ -21,32 +21,32 @@ app.post('/array', function (data, res) {
     array[4][0] *= m1
     
     var m2 = 5/((array[0][1] + array[1][1] + array[2][1] + array[3][1] + array[4][1])/5)
-    array[0][1] *= m1
-    array[1][1] *= m1
-    array[2][1] *= m1
-    array[3][1] *= m1
-    array[4][1] *= m1
+    array[0][1] *= m2
+    array[1][1] *= m2
+    array[2][1] *= m2
+    array[3][1] *= m2
+    array[4][1] *= m2
     
     var m3 = 5/((array[0][2] + array[1][2] + array[2][2] + array[3][2] + array[4][2])/5)
-    array[0][2] *= m1
-    array[1][2] *= m1
-    array[2][2] *= m1
-    array[3][2] *= m1
-    array[4][2] *= m1
+    array[0][2] *= m3
+    array[1][2] *= m3
+    array[2][2] *= m3
+    array[3][2] *= m3
+    array[4][2] *= m3
     
     var m4 = 5/((array[0][3] + array[1][3] + array[2][3] + array[3][3] + array[4][3])/5)
-    array[0][3] *= m1
-    array[1][3] *= m1
-    array[2][3] *= m1
-    array[3][3] *= m1
-    array[4][3] *= m1
+    array[0][3] *= m4
+    array[1][3] *= m4
+    array[2][3] *= m4
+    array[3][3] *= m4
+    array[4][3] *= m4
     
     var m5 = 5/((array[0][4] + array[1][4] + array[2][4] + array[3][4] + array[4][4])/5)
-    array[0][4] *= m1
-    array[1][4] *= m1
-    array[2][4] *= m1
-    array[3][4] *= m1
-    array[4][4] *= m1
+    array[0][4] *= m5
+    array[1][4] *= m5
+    array[2][4] *= m5
+    array[3][4] *= m5
+    array[4][4] *= m5
     
     console.log(array)
     
